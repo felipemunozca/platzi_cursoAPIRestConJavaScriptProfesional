9ae16f0650b35a69fe5ecdab11bdd7897e7e719b
Felipe Muñoz <analista.felipe.m@gmail.com>
2025-02-08 00:06:24 -0300

    Clase 11 completada.

--- codigoConComentarios/clase11_eventoScroll/main.js
+++ codigoConComentarios/clase11_eventoScroll/main.js
@@ -143,13 +143,57 @@ async function getTrendingMovies() {
     const movies = data.results;
 
     createMovies(movies, genericSection, {lazyLoad: true, clean: true});
+
+    /**
+     * N11.6: Se quita el botón para "Cargar más" películas, ya que ahora se hará mediante un evento.
+     */
+    // const btnLoadMore = document.createElement('button');
+    // btnLoadMore.innerText = "Cargar más";
+    // btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+    // genericSection.appendChild(btnLoadMore);
 }
 
+/**
+ * N11.5: De forma momentánea, se crea un escuchador de eventos que estará atento al scroll y asi poder ejecutar la función para 
+ *      paginar las películas y asi probar si el código quedo bien creado.
+ * Recordar que antes la función se ejecutaba al presionar un botón, ese botón se eliminara.
+ * 
+ * N11.7: Para crear un código mas optimo, se creara una nueva forma de llamar la función getPaginatedTrendingMovies() dentro del 
+ *      archivo navigation.js por lo que este evento scroll quedara comentado.
+ */
+// let page = 1;
+// window.addEventListener('scroll', getPaginatedTrendingMovies);
+
 async function getPaginatedTrendingMovies() {
+    /**
+     * N11.1: En esta clase se continuara con el desarrollo del scroll infinito, lo primero sera comprender los siguientes términos.
+     * Dentro de la consola del navegador, se puede utilizar la propiedad "document.documentElement." para obtener los siguientes 
+     *      valores:
+     * - scrollTop: es la propiedad que nos dice cuanto scroll hemos hecho en la pantalla.
+     * - clientHeight: es la propiedad para obtener el alto del dispositivo en pixeles.
+     * - scrollHeight: es cuanto scroll se puede hacer en esa pantalla (la suma de las dos propiedades anteriores).
+     * 
+     * N11.2: se crea una constate y se desestructuran todo lo que venga de document.documentElement.
+     * Asi obtendremos los valores de scrollTop, scrollHeight y clientHeight.
+     */
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
+    /**
+     * N11.3: Se crea una nueva constante para almacenar el valor si se llega al final de la pantalla.
+     * Se toma el valor del scroll y se le suma el tamaño de la pantalla, si es mayor o igual al scroll máximo que se puede hacer 
+     *      se guardara un valor "true" o "false".
+     * Llegar al fondo o footer de la pagina es muy complicado, por lo que un truco que se puede hacer, es restarle una cantidad de 
+     *      pixeles para que asi si se pueda llegar al fondo, en este caso se le restan 15 pixeles del fondo (pueden ser mas o menos,
+     *      dependerá de que se esta desarrollando y de la interacción que se busca obtener del usuario).
+     */
     const scrollIsBottom = (scrollTop + clientHeight) >= (scrollHeight - 15);
 
+    /**
+     * N11.4: Se crea una validación. Si scrollIsBottom es igual a "true" se ejecuta el código para llamar a la siguiente pagina de 
+     *      películas.
+     * No es necesario agregar el valor "== true" ya que por defecto, los valores de las variables son true, no es necesario pedir 
+     *      una confirmación.
+     */
     if (scrollIsBottom) {
         page++;
         const { data } = await api('trending/movie/day', {
--- codigoConComentarios/clase11_eventoScroll/navigation.js
+++ codigoConComentarios/clase11_eventoScroll/navigation.js
@@ -1,3 +1,9 @@
+/**
+ * N11.8: Se crea la variable para comenzar la paginación en la pagina 1.
+ * Se crea una nueva variable que llamara infiniteScroll y como indica el nombre, se ejecutara cada vez que se quieran cargar 
+ *      mas películas. La variable no tendrá valor ya que este se obtendrá cuando se ejecute un llamado de las funciones que se 
+ *      llamen "paginated" y asi se obtendrá un valor dinámico y re-utilizable.
+ */
 let page = 1;
 let infiniteScroll;
 
@@ -32,9 +38,29 @@ arrowBtn.addEventListener('click', () => {
 
 window.addEventListener('DOMContentLoaded', navigator, false);
 window.addEventListener('hashchange', navigator, false);
+/**
+ * N11.9: Se crea el escuchador de eventos que estará escuchando al "scroll" y se ejecutara cada vez que se llame en diferentes 
+ *      secciones de nuestra pagina.
+ */
 window.addEventListener('scroll', infiniteScroll, false);
 
 function navigator() {
+    /**
+     * N11.11: Si se ejecuta el código como esta hasta el punto 11.10 no sucederá nada, esto se debe a que la variable 
+     *      infiniteScroll NO TIENE ningún valor cuando se declaro, y no se entera nunca que se le ha asignado un nuevo valor en 
+     *      la función trendsPage(), por lo que se debe crear una validación.
+     * Si infiniteScroll tiene algún valor, el que sea, se utiliza la propiedad removeEventListener() para quitar el evento "scroll" 
+     *      y ya no sera igual a la variable infiniteScroll.
+     * Luego se le asigna el valor de indefinido a la variable infiniteScroll. ¿Por que se debe asignar el valor de undefined? eso 
+     *      se debe a que como sera una valor dinámico, cada vez que se ejecute la función navigator() podamos quitar ese evento y 
+     *      luego cambiarlo. Asi cada vez que se llame a cada uno de los hash, se le estará asignando un valor a infiniteScroll.
+     * 
+     * N11.13: Dentro del evento se agrega un tercer parámetro que se declara como un objeto.
+     * Passive lo que hace es evitar el llamado de preventDefault() en el caso de que este existiese en la función llamada por el 
+     *      Listener. En los navegadores que usa la gente normal el valor por defecto es false por lo que no siempre se utiliza, 
+     *      pero en el caso de "Safari" e "Internet Explorer" el valor por defecto es true. Por lo que es recomendable ponerle un 
+     *      valor para que el código se ejecute igual en todos los navegadores.
+     */
     if (infiniteScroll) {
         window.removeEventListener('scroll', infiniteScroll, { passive: false });
         infiniteScroll = undefined;
@@ -55,6 +81,13 @@ function navigator() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 
+    /**
+     * N11.12: Para complementar la lógica del punto 11.11 se crea una nueva validación.
+     * SI infiniteScroll tiene algún valor, se utiliza la propiedad addEventListener() para agregar el evento "scroll" y se le 
+     *      asigna ese valor a infiniteScroll.
+     * EN RESUMEN: el primer if() quita el evento scroll, luego se cambia la vista utilizando los hash y el segundo if() agrega 
+     *      el evento scroll.
+     */
     if (infiniteScroll) {
         window.addEventListener('scroll', infiniteScroll, { passive: false });
     }
@@ -171,5 +204,10 @@ function trendsPage() {
     headerCategoryTitle.innerHTML = 'Top 20 películas en Tendencias'; //Tendencias
     getTrendingMovies();
 
+    /**
+     * N11.10: Se le asigna el valor de la función getPaginatedTrendingMovies() a la variable infiniteScroll, pero 
+     *      MUY IMPORTANTE no se le agregan los paréntesis () para que la función no se ejecute al cargar, sino que se ejecutara 
+     *      cuando se ejecute el evento scroll y llegue al final de la pagina.
+     */
     infiniteScroll = getPaginatedTrendingMovies;
 }
